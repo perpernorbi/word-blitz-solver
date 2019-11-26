4f14994e6b73c873f038972d6083944050029a0b
Norbert Koszo <k.norbi@gmail.com>
2019-11-26 21:25:32 +0100

    Add period to mark cell as special

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <math.h>
-#include <set>
+#include <vector>
 
 int get_size(icu::UnicodeString s) {
   int size = sqrt(s.length());
@@ -36,12 +37,40 @@ public:
       : grid_(grid), word_list_(word_list_), chars_(chars) {}
 };
 
+std::pair<icu::UnicodeString, std::vector<bool>>
+GetCharsAndStars(std::string input) {
+  icu::UnicodeString utf(icu::UnicodeString::fromUTF8(input));
+  icu::UnicodeString chars;
+  std::vector<bool> stars(utf.length(), false);
+  int i = 0;
+  for (int j = 0; j < utf.length(); ++j)
+    if (utf[j] == '.')
+      stars[i] = true;
+    else {
+      chars += utf[j];
+      ++i;
+    }
+  return std::make_pair(chars, stars);
+}
+
+bool IsStarred(std::vector<bool> already_visited, std::vector<bool> stars) {
+  for (auto i = 0; i < already_visited.size(); ++i)
+    if (already_visited[i] && stars[i])
+      return true;
+  return false;
+}
+
+enum class Printed { yes, no };
+
 int main(int argc, char *argv[]) {
   WordList word_list = CreateWordList("freedict_utf8");
   if (argc != 2)
     throw std::runtime_error(std::string{"usage: "} + argv[0] + " characters");
-  const auto chars = icu::UnicodeString::fromUTF8(argv[1]);
+  const auto chars_and_stars = GetCharsAndStars(argv[1]);
+  const auto &chars = chars_and_stars.first;
+  const auto &stars = chars_and_stars.second;
   const auto size = get_size(chars);
+
   std::cout << "size: " << size << std::endl;
 
   Grid grid(size);
@@ -52,7 +81,7 @@ int main(int argc, char *argv[]) {
                         icu::UnicodeString::fromUTF8("")});
 
   std::vector<bool> already_visited(size * size, false);
-  std::set<icu::UnicodeString> already_found;
+  std::map<icu::UnicodeString, Printed> already_found;
   while (state.size() > 0) {
     State &current_state = state.back();
 
@@ -63,7 +92,8 @@ int main(int argc, char *argv[]) {
       // No more possible moves from there
       already_visited[current_state.position] = false;
       state.pop_back();
-      ++state.back().m_begin;
+      if (state.size() > 0)
+        ++state.back().m_begin;
       continue;
     }
 
@@ -72,7 +102,6 @@ int main(int argc, char *argv[]) {
     already_visited[new_state.position] = true;
     new_state.word = current_state.word;
     new_state.word.append(chars[new_state.position]);
-    // std::cout << "word " << new_state.word << std::endl;
     new_state.w_begin = std::find_if(current_state.w_begin, current_state.w_end,
                                      [new_state](const icu::UnicodeString &s) {
                                        return s.startsWith(new_state.word);
@@ -82,25 +111,30 @@ int main(int argc, char *argv[]) {
                          [new_state](const icu::UnicodeString &s) {
                            return s.startsWith(new_state.word);
                          });
-    // std::cout << "begin " << *new_state.w_begin << std::endl;
-    // std::cout << "end " << *new_state.w_end << std::endl;
     new_state.m_begin = grid.PossibleMoves(new_state.position).cbegin();
     new_state.m_end = grid.PossibleMoves(new_state.position).cend();
     state.push_back(new_state);
     if (new_state.word == *new_state.w_begin) {
-      if (std::find(already_found.cbegin(), already_found.cend(),
-                    new_state.word) == already_found.cend()) {
-        already_found.insert(new_state.word);
-        std::cout << new_state.word << std::endl;
+      if (already_found.find(new_state.word) == already_found.cend()) {
+        const auto starred = IsStarred(already_visited, stars);
+        already_found.insert(
+            {new_state.word, starred ? Printed::yes : Printed::no});
+        if (starred)
+          std::cout << "* " << state[1].position << " " << new_state.word
+                    << std::endl;
       }
     }
     if (new_state.w_begin == new_state.w_end) {
       already_visited[new_state.position] = false;
       state.pop_back();
-      ++state.back().m_begin;
+      if (state.size() > 0)
+        ++state.back().m_begin;
       continue;
     }
   }
 
+  for (auto result : already_found)
+    if (result.second == Printed::no)
+      std::cout << result.first << std::endl;
   return 0;
 }
